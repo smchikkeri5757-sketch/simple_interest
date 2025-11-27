dfde1f28eeb74f129b056eaa715df6010f7b8dd2
gouri <smchikkeri5757@gmail.com>
2025-11-27 13:49:01 +0530

    Update simple_interest.py

--- simple_interest.py
+++ simple_interest.py
@@ -1,8 +1,15 @@
-# simple_interest.py
-principal = float(input("Enter the Principal amount: "))
-rate = float(input("Enter the Rate of Interest (%): "))
-time = float(input("Enter the Time (in years): "))
-# Calculate Simple Interest
+import sys
+if len(sys.argv) != 4:
+    print("ERROR: Please provide Principal, Rate of Interest, and Time as parameters.")
+    print("Usage: python3 simple_interest.py <PRINCIPAL> <RATE> <TIME>")
+    sys.exit(1)
+principal = float(sys.argv[1])
+rate = float(sys.argv[2])
+time = float(sys.argv[3])
+print("\n=== INPUT RECEIVED FROM JENKINS PARAMETERS ===")
+print("Principal:", principal)
+print("Rate of Interest:", rate)
+print("Time:", time)
 simple_interest = (principal * rate * time) / 100
-# Display result
+print("\n===== RESULT =====")
 print("Simple Interest:", simple_interest)
